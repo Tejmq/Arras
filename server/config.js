75ae3dfcf2fb62d52da625114a631a4c6eadeae0
Tejmq <75015175+Tejmq@users.noreply.github.com>
2026-04-03 14:36:35 +0300

    attempt

--- server/config.js
+++ server/config.js
@@ -1,11 +1,12 @@
 module.exports = {
+    // Open Source Arras
     version: 'v2.0.11.3',
     devBuild: false,
 
     // Client
     main_menu: 'index.html',
-    host: '0.0.0.0',
-    port: process.env.PORT, // Use Render's dynamic port
+    host: '0.0.0.0', // Render-friendly host
+    port: process.env.PORT || 3000, // Render-friendly port
 
     // Server
     visible_list_interval: 250,
@@ -16,19 +17,18 @@ module.exports = {
         {
             share_client_server: false,
             host: '0.0.0.0',
-            port: process.env.PORT,
+            port: process.env.PORT || 3000,
             id: 'main',
             region: "Local",
-            gamemode: ['ffa'],          // base gamemode only
+            gamemode: ['ffa'],
             player_cap: 80,
             featured: false,
             unlisted: false,
             private: false,
             properties: {
                 teams: 4,
-                bot_cap: 0,
-            },
-            modifiers: ['arms_race'], // modifiers go here
+                bot_cap: 40
+            }
         }
     ],
 
@@ -43,7 +43,6 @@ module.exports = {
                    "You will be invulnerable until you move or shoot.\n" +
                    "Please report any bugs you encounter!",
     token_message: "Friendly reminder: Please do not repeatedly kill others with an overpowered tank.",
-
     chat_message_duration: 15_000,
     popup_message_duration: 10_000,
     sanitize_chat_input: true,
@@ -84,7 +83,6 @@ module.exports = {
     bot_skill_upgrade_chances: [1,1,3,4,4,4,4,2,1,1],
     bot_class_upgrade_chances: [1,5,20,37,37],
     bot_name_prefix: "[AI] ",
-
     spawn_class: 'basic',
     regenerate_tick: 100,
 
@@ -94,13 +92,48 @@ module.exports = {
     enemy_cap_nest: 10,
     food_group_cap: 6,
 
+    // Keep your old food configuration
+    food_types: Array(3).fill().map((_, i, a) => [
+        4 ** (a.length - i),
+        Array(3).fill().map((_, j, b) => [
+            5 ** (b.length - j),
+            Array(6).fill().map((_, k, c) => [
+                k ? 10 ** (c.length - k - 1) : 200_000_000,
+                [
+                    [24, `laby_${i}_${j}_${k}_0`]
+                ]
+            ])
+        ])
+    ]),
+    food_types_nest: Array(2).fill().map((_, i, a) => [
+        4 ** (a.length - i),
+        Array(3).fill().map((_, j, b) => [
+            5 ** (b.length - j),
+            Array(6).fill().map((_, k, c) => [
+                k ? 10 ** (c.length - k - 1) : 200_000_000,
+                [
+                    [24, `laby_${i+3}_${j}_${k}_0`]
+                ]
+            ])
+        ])
+    ]),
+
+    classic_food: false,
+    classic_food_types: [
+        [1, [[65, 'egg'], [64, 'triangle'], [45, 'square'], [7, 'pentagon']]],
+        [1/50000, [[625,'gem'],[125,'shinyTriangle'],[25,'shinySquare'],[5,'shinyPentagon']]],
+        [1/1000000, [[1296,'jewel'],[216,'legendaryTriangle'],[36,'legendarySquare'],[6,'legendaryPentagon']]]
+    ],
+    classic_food_types_nest: [[1, [[16,'pentagon'],[4,'betaPentagon'],[1,'alphaPentagon']]]],
+    classic_enemy_types_nest: [[1, [[1,'crasher']]], [1/20, [[1,'sentryGun'],[1,'sentrySwarm'],[1,'sentryTrap']]]],
+
     enable_bosses: true,
     boss_spawn_cooldown: 260,
     boss_spawn_delay: 6,
     boss_types: [
-        { bosses: ['eliteDestroyer','eliteGunner','eliteSprayer','eliteBattleship','eliteSpawner'], amount: [5,5,4,2,1], chance: 2, nameType: 'a' },
-        { bosses: ['roguePalisade'], amount: [4,1], chance: 1, nameType: 'castle', message: 'A strange trembling...' },
-        { bosses: ['summoner','eliteSkimmer','nestKeeper'], amount: [2,2,1], chance: 1, nameType: 'a', message: 'A strange trembling...' }
+        { bosses: ['eliteDestroyer','eliteGunner','eliteSprayer','eliteBattleship','eliteSpawner'], amount:[5,5,4,2,1], chance:2, nameType:'a' },
+        { bosses: ['roguePalisade'], amount:[4,1], chance:1, nameType:'castle', message:'A strange trembling...' },
+        { bosses: ['summoner','eliteSkimmer','nestKeeper'], amount:[2,2,1], chance:1, nameType:'a', message:'A strange trembling...' }
     ],
 
     team_weights: {},
@@ -109,8 +142,7 @@ module.exports = {
 
     gamemode_name_prefixes: [],
     arena_shape: 'rect',
-    arms_race: true, // enabled via modifier
-    siege: true,     // enabled via modifier
+    arms_race: false,
     blackout: false,
     clan_wars: false,
     diep: false,
@@ -121,6 +153,7 @@ module.exports = {
     mode: 'ffa',
     mothership: false,
     retrograde: false,
+    siege: false,
     space_physics: false,
     spawn_confinement: {},
     tag: false,
