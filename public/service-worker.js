5968bd00af3de5a019610f3cc161acb778c510f5
Tejmq <75015175+Tejmq@users.noreply.github.com>
2026-04-03 12:41:45 +0300

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,7 @@
+// Add install listener
 self.addEventListener("install", () => self.skipWaiting());
+// And activate one
 self.addEventListener("activate", async () => {
     await self.registration.unregister();
-    for (var e of await self.clients.matchAll({ type: "window" })) e.navigate(e.url)
+    for (var e of await self.clients.matchAll({ type: "window" })) e.navigate(e.url);
 });
